b2d11fa4881466c9c9c078b9455726449b42ac0f
t.rapp <tobias.rapp@irix.ch>
2020-09-20 14:34:43 +0200

    WeekTarget zu WeekTargetList umbenannt

--- todolist/models.py
+++ todolist/models.py
@@ -29,7 +29,7 @@ class TodoList(models.Model): #Todolist able name that inherits models.Model
 	def __str__(self):
 		return self.title #name to be shown when called
 
-class WeekTarget(models.Model):
+class WeekTargetList(models.Model):
 	show_in_number_of_week_cycles = models.IntegerField(null=True)		#models.IntegerField(null=True)
 	is_during_working_hours = models.IntegerField
 	day_target_show_from_weekday = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
--- todolist/views.py
+++ todolist/views.py
@@ -2,14 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render,redirect
-from .models import TodoList, Category, WeekTarget
+from .models import TodoList, Category, WeekTargetList
 import datetime
 # Create your views here.
 
 def index(request): #the index view
 	todos = TodoList.objects.all() #quering all todos with the object manager
 	categories = Category.objects.all() #getting all categories with object manager
-	weektargets = WeekTarget.objects.all() #getting all week targets with object manager
+	weektargets = WeekTargetList.objects.all() #getting all week targets with object manager
 	if request.method == "POST": #checking if the request method is a POST
 		if "taskAdd" in request.POST: #checking if there is a request to add a todo
 			title = request.POST["description"] #title
@@ -25,6 +25,22 @@ def index(request): #the index view
 			for todo_id in checkedlist:
 				todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
 				todo.delete() #deleting todo
+
+		if "WeekTargetAdd" in request.POST: #checking if there is a request to add a todo
+			title = request.POST["week_target_description"]
+			date = str(request.POST["date"]) #date
+			category = request.POST["category_select"] #category
+			content = title + " -- " + date + " " + category #content
+			Todo = TodoList(title=title, content=content, due_date=date, category=Category.objects.get(name=category))
+			Todo.save() #saving the todo 
+			return redirect("/") #reloading the page
+		
+		if "WeekTargetDelete" in request.POST: #checking if there is a request to delete a todo
+			checkedlist = request.POST["checkedbox"] #checked todos to be deleted
+			for todo_id in checkedlist:
+				todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
+				todo.delete() #deleting todo
+	
 
 	return render(request, "index.html", {"todos": todos, "categories":categories})
 
